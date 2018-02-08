add16955a534946e96c937d9c59534e74878826e
Glitch (request-header-node) <none>
2018-02-08 16:05:11 +0000

    Exporting to Omerdogan3/Request-Header

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+  app.route('/whoami')
+    .get(function(req, res) {
+      var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     var info = {
+         'ipaddress': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+     };
+     res.send(info);
+    });
+}; 
